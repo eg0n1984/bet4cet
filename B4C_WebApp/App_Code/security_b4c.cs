2535868b1493e75893fd22174482daa5738176a9
nunotrancoso <erazerpt@gmail.com>
2017-07-09 13:41:54 +0100

    Mudança de uppercase hex para lowercase hex, para ser compativel com a library de js. Não era critico, mas fica masi elegante.

--- B4C_WebApp/App_Code/security_b4c.cs
+++ B4C_WebApp/App_Code/security_b4c.cs
@@ -19,7 +19,7 @@ namespace security_b4c
 			byte[] hash = md5.ComputeHash(inputBytes);
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < hash.Length; i++)
-			{ sb.Append(hash[i].ToString("X2")); }
+			{ sb.Append(hash[i].ToString("x2")); }
 			return sb.ToString();
 		}
 	}
